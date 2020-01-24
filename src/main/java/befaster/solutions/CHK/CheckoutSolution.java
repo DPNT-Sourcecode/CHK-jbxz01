2857198d1fedfbad443bcfc51bfdf1725d571c2c
sbx_user1051 <sbx_user1051@169.254.228.181>
2020-01-24 19:13:22 +0000

    Fri Jan 24 19:13:22 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,12 +5,12 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
-        int[] numberOfItems = new int[5];
+        int[] numberOfItems = new int[6];
 
         for (int i = 0; i < skus.length(); i++)
         {
             Character character = skus.charAt(i);
-            if (character < 65 || character > 69 )
+            if (character < 65 || character > 70 )
                 return -1;
             numberOfItems[character - 65]++;
         }
@@ -35,10 +35,15 @@ public class CheckoutSolution {
         totalCheckout += numberOfItems[2] * 20;
         //Adding the value of items D
         totalCheckout += numberOfItems[3] * 15;
-        //Adding the valoe of items E
+        //Adding the value of items E
         totalCheckout += numberOfItems[4] * 40;
+
+        //Adding the value of items F
+        totalCheckout += (numberOfItems[5] / 3) * 80; //80 = 2 * F, so we charge 3 items for the price of 2
+        totalCheckout += (numberOfItems[5] % 3) * 40;
 
         return totalCheckout;
     }
 }
+
 
