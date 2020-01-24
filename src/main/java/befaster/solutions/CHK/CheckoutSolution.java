0d318fa30b520293d30be95fb1667f0924c4d552
sbx_user1051 <sbx_user1051@169.254.228.181>
2020-01-24 18:52:46 +0000

    Fri Jan 24 18:52:46 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,9 +7,20 @@ public class CheckoutSolution {
 
         int[] numberOfItems = new int[4];
 
+        for (int i = 0; i < skus.length(); i++)
+        {
+            Character character = skus.charAt(i);
+            if (character < 65 || character > 68 )
+                return -1;
+            numberOfItems[character - 65]++;
+        }
+
+        int totalCheckout = 0;
+
         System.out.println(skus);
 
         return -1;
     }
 }
+
 
